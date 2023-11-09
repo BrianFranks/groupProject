c216ecf5b311c1a891280b061805a34db468dcbd
Blendedpencil <129638967+Blendedpencil@users.noreply.github.com>
2023-11-09 08:28:38 -0600

    What do yall think of this way of structuring the project?

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 #this is the main file and will connect everything
+class inventory:
+    items = []
+    def addItem(self, itemAdded):
+        self.items.append(itemAdded)
 print("Hello World!")
+
+#inventory class
+
+
+#main
+position = "employee"
+storeInventory = inventory()
+choice = input("Would you like to:\n0:Add a new item to inventory?\nChoice: ")
+if choice == "0" and position == "employee":
+    itemAdded = input("What would you like to add? ")
+    storeInventory.addItem(itemAdded)
