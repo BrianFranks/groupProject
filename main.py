f80f8c033089548c57bdecc20fe9616a7857eee5
Dean Patterson <129638967+Blendedpencil@users.noreply.github.com>
2023-11-16 13:33:41 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,1 @@
-#inventory
-class inventory:
-    items = []
-    def addItem(self, itemAdded):
-        self.items.append(itemAdded)
 
-#cart
-
-#person
-
-#main
-position = "employee"
-storeInventory = inventory()
-choice = input("Would you like to:\n0:Add a new item to inventory?\nChoice: ")
-if choice == "0" and position == "employee":
-    itemAdded = input("What would you like to add? ")
-    storeInventory.addItem(itemAdded)
