a3abeabe9ab764d5e4a674d1729a3567af15a7c3
dorwinj2001 <149213146+dorwinj2001@users.noreply.github.com>
2023-11-28 14:25:21 -0600

    Create user.sql
    adds the user table

---
+++ user.sql
@@ -0,0 +1,25 @@
+try:
+    cursor.execute(f"INSERT INTO user (Email, Password, firstName, LastName, Address, City, State, Zip, Payment) VALUES ('{user.email}', '{user.password}', '{user.firstName}', '{user.lastName}', '{user.address}', '{user.city}', '{user.state}', '{user.zipCode}', '{user.payment}')")
+
+    connection.commit()
+    print("Record inserted successfully.")
+except sqlite3.Error as e:
+    print("Error inserting record:", e)
+
+
+   
+
+try:
+    connection = sqlite3.connect(".db")
+
+    print("Successful connection.")
+
+except:
+    print("Failed connection.")
+
+    ## exits the program if unsuccessful
+    sys.exit()
+
+## cursor to send queries through
+cursor = connection.cursor()
+    
